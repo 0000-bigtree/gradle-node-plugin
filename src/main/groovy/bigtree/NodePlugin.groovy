faca6f6818a43a2e89349641f045b2cb763c2f90
0000-bigtree <0000.bigtree@gmail.com>
2015-01-15 17:03:59 +0800

    完成基本的task installNode reinstallNode, uninstallNode, newProject, npm, exec

--- src/main/groovy/bigtree/NodePlugin.groovy
+++ src/main/groovy/bigtree/NodePlugin.groovy
@@ -22,32 +22,19 @@ class NodePlugin implements Plugin<Project> {
     }      
     
     project.task('installNode') << {
-      description 'Install Node'
-      if(isWindows()) {
-        installNodeOnWindows(project) 
-      } else if(isOsX()) {
-        installNodeOnOsX(project)
-      } else if(isLinux()) {        
-      } else {
-        throw new UnsupportedOperationException('DO NOT support this platform')        
-      }
-      // setExecutable(project)
-      // changeToDefaultGemSource(project)
-      installDefaultGlobalModules(project)
+      description 'Install Node' 
+      installNode(project)      
     }
     
     project.task('reinstallNode') << {
       description 'Reinstall Node, will delete installed Node previously'
-      deleteRubyHome(project)    
-      extractDistr(project)
-      setExecutable(project)
-      changeToDefaultGemSource(project)
-      installDefaultGems(project)
+      uninstallNode(project)
+      installNode(project)
     }    
     
     project.task('uninstallNode') << {
-      description 'Uninstall Node, will delete installed Node'        
-      deleteRubyHome(project)    
+      description 'Uninstall Node, will delete installed Node'
+      uninstallNode(project)
     }
     
     project.task('exec') << {  
@@ -55,7 +42,7 @@ class NodePlugin implements Plugin<Project> {
         def cmds = project.cmds
         cmds.split(';').each {
           def cmd = "${it}"
-          exec(project, cmd)
+          exec(project, project.nodeProject.nameWithPath, cmd)
         }          
       }
     }
@@ -63,142 +50,45 @@ class NodePlugin implements Plugin<Project> {
     project.task('npm') << {
       def args = project.hasProperty('args') ? project.args : ''
       if (null == args || 0 >= args.length()) {
-        args = 'list -g'
+        args = 'list'
       }
       def cmd = "${args}"
       def executable = getNpmExecutableWithPath(project)
-      project.ant.exec(dir: project.nodeEnv.getNodeHome(), 
+      project.ant.exec(dir: project.nodeProject.nameWithPath, 
                        executable: executable) {
                          arg(line: cmd)      
       }
-    }     
-    
-    /*    
-    project.task('addOfficialGemSource') << {
-    addOrRemoveGemSource(project, true, project.rubyEnv.officialGemSource)    
-    }
-    
-    project.task('addGemSource') << {  
-    if (project.hasProperty('gemSource')) {
-    final fromArg = project.gemSource
-    if (null != fromArg && 0 < fromArg.length()) {
-    addOrRemoveGemSource(project, true, fromArg)
-    }
-    }      
-    }
-    
-    project.task('removeGemSource') << {  
-    if (project.hasProperty('gemSource')) {
-    final fromArg = project.gemSource
-    if (null != fromArg && 0 < fromArg.length()) {
-    addOrRemoveGemSource(project, false, fromArg)
-    }
-    }      
-    }
-    
+    } 
+
     project.task('newProject') << {  
-    def gems = project.rubyProject.defaultGems
-    if (null != gems && 0 < gems.length()) {
-    installGems(project, gems)
-    }
-    if (project.rubyProject.isRailsProject) {
-    // 如果是 rails 项目，利用 rails new 命令来创建项目
-    def cmd = "-S rails new ${project.rubyProject.nameWithPath} --skip-bundle ${project.rubyProject.railsNewArgs}"
-    def executable = getRubyExecutableWithPath(project)
-    ant.exec(executable: executable) {
-    env(key: 'HOME', value: project.rubyEnv.rubyHome)
-    env(key: 'JRUBY_HOME', value: project.rubyEnv.rubyHome)
-    arg(line: cmd)
-    } 
-    // 将 rails 生成的 Gemfile中的 Gem Source 替换为指定的 Source
-    def source = project.rubyProject.gemfileSource 
-    if (null == source || 0 >= source.length()) {
-    source = project.rubyEnv.defaultGemSource
-    } 
-    if (null == source || 0 >= source.length()) {
-    source = project.rubyEnv.officialGemSource
-    }
-    replaceGemfileSource(project, "${project.rubyProject.nameWithPath}/Gemfile", source)
-    
-    // 执行 bundle，安装 gem
-    exec(project, "-S bundle")   
-    } else {
-    // 非 rails 项目 
-    new File(project.rubyProject.nameWithPath).mkdirs()
-    if (project.rubyProject.isCreateGemfile) {
-    createNewGemfile(project, "${project.rubyProject.nameWithPath}/Gemfile")
-    }
-    }
-    if('jruby' == project.rubyEnv.engine) {
-    // 执行 warble config，生成 warble 配置文件 
-    exec(project, "-S warble config")
-    }
-    }
-    
-    project.task('war', type: org.gradle.api.tasks.bundling.War) << {
-    // 准备好 assets
-    def cmd = "-S rake assets:clobber assets:precompile RAILS_ENV=production"
-    exec(project, cmd)
-    // 编译并打包为 war
-    // 当前的 warble 版本 1.4.4 执行 warble compiled war， 在 windows 下会报错
-    cmd = isWindows() ? "-S warble war" : "-S warble compiled war"
-    exec(project, cmd)
-    
-    // 移动到生成的 war 包到 project.buildDir
-    def f = new File(project.rubyProject.nameWithPath)
-    def name = f.name
-    name = (name[0].toLowerCase() + name.substring(1) + '.war')
-    def warFileWithPath = new File(f, name)
-    ant.move(file: warFileWithPath, tofile: "${project.buildDir}/libs/${project.name}-${project.version}.war")
-    }
-    
-    project.task('rails') << {
-    def args = project.hasProperty('args') ? project.args : ''
-    if (null == args || 0 >= args.length()) {
-    args = ''
-    }
-    def cmd = "-S rails ${args}"
-    exec(project, cmd)
-    }      
-    
-    project.task('rackup') << {
-    def args = project.hasProperty('args') ? project.args : ''
-    if (null == args || 0 >= args.length()) {
-    args = ''
-    }
-    def cmd = "-S rackup ${args}"
-    exec(project, cmd)
-    }  
-    
-    project.task('rake') << {
-    def args = project.hasProperty('args') ? project.args : ''
-    if (null == args || 0 >= args.length()) {
-    args = ''
-    }
-    def cmd = "-S rake ${args}"
-    exec(project, cmd)
-    }      
-    
-    project.task('bundle') << {
-    def args = project.hasProperty('args') ? project.args : ''
-    if (null == args || 0 >= args.length()) {
-    args = ''
-    }
-    def cmd = "-S bundle ${args}"
-    exec(project, cmd)
-    }      
-    
- 
-    
-    project.task('exec') << {  
-    if (project.hasProperty('cmds')) {
-    def cmds = project.cmds
-    cmds.split(';').each {
-    def cmd = "-S ${it}"
-    exec(project, cmd)
-    }          
-    }
-    }    */    
+      final nodeProjectDir = project.file(project.nodeProject.nameWithPath)
+      if (!nodeProjectDir.exists()) {
+        nodeProjectDir.mkdirs()
+      }
+      
+      // def cmd = 'init'
+      // def executable = getNpmExecutableWithPath(project)
+      // project.ant.exec(dir: nodeProjectDir, executable: executable, spawn: 'true') {
+      //   arg(line: cmd)      
+      // }
+      
+      final packageFile = new File(nodeProjectDir, "package.json")
+      if (!packageFile.exists()) {
+        packageFile << generatePackageJson()
+      }
+      
+      def modules = project.nodeProject.defaultModules
+      if (null == modules) {
+        modules = ""
+      }      
+      if (project.nodeProject.isNwJsProject) {
+        exec(project, project.nodeProject.nameWithPath, ('npm install node-webkit-builder -g'))
+      }
+      
+      if (0 < modules.length()) {
+        exec(project, project.nodeProject.nameWithPath, ('npm install ' + modules + ' --save'))
+      }
+    }    
   }
   
   def installNodeOnOsX(project) {
@@ -283,97 +173,7 @@ class NodePlugin implements Plugin<Project> {
     project.ant.exec(dir: "${tmpPath}/npm-${nodeEnv.npmVer}", executable: executable) {
       arg(line: 'cli.js install -g')
     }     
-  }
-  
-  /*  
-  def deleteRubyHome(project) {
-  project.ant.delete(dir: project.rubyEnv.rubyHome)
-  }
-  
-  def extractDistr(project) {
-  // 添加依赖，根据 rubyEnv.rubyDistrDependency 的值
-  project.dependencies {
-  rubyDistrDependency project.rubyEnv.rubyDistrDependency
-  } 
-  
-  // 取到最后加入的依赖，担心可能添加了多次
-  final distrFile = project.configurations.rubyDistrDependency.last() 
-  if(isWindows()) {
-  project.ant.unzip(src: distrFile.absolutePath, 
-  dest: project.rubyEnv.extractPath) 
-  } else {
-  project.ant.untar(src: distrFile.absolutePath, 
-  compression: 'gzip',
-  dest: project.rubyEnv.extractPath)           
-  }
-  }
-
-  
-  // 添加 Gem 源 URL
-  def addOrRemoveGemSource(project, isAdd, source) {
-  def flag = isAdd ? '-a' : '-r'
-  def cmd = "-S gem sources -c ${flag} ${source}"
-  def executable = getRubyExecutableWithPath(project)
-  project.ant.exec(executable: executable) {
-  env(key: 'HOME', value: project.rubyEnv.rubyHome)
-  env(key: 'JRUBY_HOME', value: project.rubyEnv.rubyHome)
-  arg(line: cmd)      
-  } 
-  }    
-  
-  def changeToDefaultGemSource(project) {
-  addOrRemoveGemSource(project, false, project.rubyEnv.officialGemSource)
-  addOrRemoveGemSource(project, true, project.rubyEnv.defaultGemSource)   
-  }
-  
-
-  
-  def installDefaultGems(project) {
-  def gems = project.rubyEnv.defaultGems
-  if (null == gems || 0 >= gems.length()) {
-  gems = ''
-  }
-  gems += ' bundler '
-  if('jruby' == project.rubyEnv.engine) {
-  gems += ' warbler '      
-  }
-  installGems(project, gems)
-  }
-  
-
-  
-  def replaceGemfileSource(project, gemfileWithPath, source) {
-  def contents = new File(gemfileWithPath).getText()
-  contents = contents.replace("rubygems.org'", "rubygems.org'${System.getProperty("line.separator")}ruby '${project.rubyEnv.rubyVer}', engine: '${project.rubyEnv.engine}', engine_version: '${project.rubyEnv.engineVer}'")
-  def newFile = new File(gemfileWithPath)
-  newFile.setText("source '${source}'")
-  newFile << System.getProperty("line.separator") 
-  newFile << "# " << contents
-  }  
-  
-  def createNewGemfile(project, gemfileWithPath) {
-  def gemfile = new File(gemfileWithPath)
-  if(!gemfile.exists()) {
-  gemfile.createNewFile()
-  }
-  gemfile.setText('')
-  def source = project.rubyProject.gemfileSource 
-  if (null == source || 0 >= source.length()) {
-  source = project.rubyEnv.defaultGemSource
-  } 
-  if (null == source || 0 >= source.length()) {
-  source = project.rubyEnv.officialGemSource
-  }    
-  gemfile << "source '${source}'"
-  gemfile << "${System.getProperty("line.separator")}ruby '${project.rubyEnv.rubyVer}', engine: '${project.rubyEnv.engine}', engine_version: '${project.rubyEnv.engineVer}'"
-  gemfile << System.getProperty("line.separator") * 2
-  gemfile << "# gem 'log4r', '~> 1.1.10'${System.getProperty("line.separator")}"
-  gemfile << "# gem 'puma', '~> 2.10.2'${System.getProperty("line.separator")}"    
-  }
-  
-
-  */
-  
+  }   
   
   // *nix下，要设置这些脚本的可执行权限
   def setExecutable(project) {
@@ -411,17 +211,38 @@ class NodePlugin implements Plugin<Project> {
     }
   }  
   
-  def exec(project, cmd) {
+  def exec(project, path, cmd) {
     final cmdWithArgs = cmd.split()
     def args = ''
     if (1 < cmdWithArgs.length) {
       args = cmdWithArgs[1..-1].join(' ')
     }
     def executable = getExecutableWithPath(project, cmdWithArgs[0])
-    project.ant.exec(dir: project.nodeEnv.getNodeHome(), 
+    project.ant.exec(dir: path, 
                      executable: executable) {
                        arg(line: args)
     }    
+  }
+  
+  def installNode(project) {
+    if(isWindows()) {
+      installNodeOnWindows(project) 
+    } else if(isOsX()) {
+      installNodeOnOsX(project)
+    } else if(isLinux()) { 
+      throw new UnsupportedOperationException('DO NOT support this platform')        
+    } else {
+      throw new UnsupportedOperationException('DO NOT support this platform')        
+    }
+    installDefaultGlobalModules(project)
+  }
+  
+  def uninstallNode(project) {  
+    if(!project.hasProperty('cleanAllNode')) {
+      project.ant.delete(dir: project.nodeEnv.getNodeHome())
+    } else {
+      project.ant.delete(dir: project.nodeEnv.installPath)
+    }      
   }
   
   static isWindows() {
@@ -438,5 +259,25 @@ class NodePlugin implements Plugin<Project> {
   
   static is32Arch(ant) {
     'x86' == ant.properties['os.arch']
+  }
+  
+  // 生成 package.json 文件内容
+  static generatePackageJson() {
+"""{
+  "name": "TODOProjectName",
+  "private": true,
+  "version": "0.0.1",
+  "description": "TODO project description",
+  "keywords": [],
+  "dependencies": {
+  },
+  "devDependencies": {
+  },
+  "scripts": {
+    "start": "node app.js"
+  },
+  "main": "app.js"
+}
+"""
   }
 }
--- src/main/groovy/bigtree/NodeProjectExtension.groovy
+++ src/main/groovy/bigtree/NodeProjectExtension.groovy
@@ -6,13 +6,22 @@ class NodeProjectExtension {
   
   String nameWithPath
     
-  boolean isNodeWebkitProject = false  
+  // 是否为 nw.js(node-webkit) 项目
+  boolean isNwJsProject = false  
   
-  String nodeWebkitVer = ''
+  // nw.js 版本
+  String nwJsVer = '0.11.5'
   
   String defaultModules = ''
   
   //
   Project project 
+  
+  def getNameWithPath() {
+    if (null == nameWithPath || 0 >= nameWithPath.length()) {
+      return 'src/main/javaScript/nodeProject'
+    }
+    nameWithPath
+  }  
   
 }
